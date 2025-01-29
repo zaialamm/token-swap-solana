177dbd2a1f0cf090d64d13daa46b8903bcc55b5d
Zai <zai.alam51@gmail.com>
2025-01-29 17:11:44 +0800

    initial commit

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,32 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6jnAqptrpuhcMQphYZRHimYZHvDhRgX4QNynaSe13Nh5");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn make_offer(
+    ctx: Context<MakeOffer>,
+    id: u64,
+    token_a_offered_amount: u64,
+    token_b_wanted_amount: u64,
+    ) -> Result<()> {
+    instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+    instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
+    instructions::take_offer::send_wanted_tokens_to_maker(&ctx)?;
+    instructions::take_offer::withdraw_and_close_vault(&ctx)
+    }
+}
